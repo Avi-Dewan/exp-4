5dc98c4719588ef6a93c95e05164745213105d96
Avi Dewan <1905120@ugrad.cse.buet.ac.bd>
2024-10-03 12:47:20 +0600

    first

--- data/simCLRloader.py
+++ data/simCLRloader.py
@@ -4,21 +4,12 @@ import torch.utils.data as data
 import torchvision
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-# from Places205 import Places205
-import numpy as np
-import random
-from torch.utils.data.dataloader import default_collate
 from PIL import Image
 import os
-import errno
 import numpy as np
-import sys
-import csv
-from tqdm import tqdm
 
 
 class ContrastiveTransformations(object):
-
     def __init__(self, base_transforms, n_views=2):
         self.base_transforms = base_transforms
         self.n_views = n_views
@@ -26,16 +17,17 @@ class ContrastiveTransformations(object):
     def __call__(self, x):
         return [self.base_transforms(x) for i in range(self.n_views)]
 
+
 class SimCLRDataset(data.Dataset):
     def __init__(self, dataset_name, split, dataset_root=None):
         self.split = split.lower()
         self.dataset_name = dataset_name.lower()
         self.name = self.dataset_name + '_' + self.split
-        
+
         if self.dataset_name == 'cifar10':
-            self.mean_pix = [x/255.0 for x in [125.3, 123.0, 113.9]]
-            self.std_pix = [x/255.0 for x in [63.0, 62.1, 66.7]]
-            
+            self.mean_pix = [x / 255.0 for x in [125.3, 123.0, 113.9]]
+            self.std_pix = [x / 255.0 for x in [63.0, 62.1, 66.7]]
+
             if split != 'test':
                 self.transform = transforms.Compose([
                     transforms.RandomHorizontalFlip(0.5),
@@ -54,20 +46,20 @@ class SimCLRDataset(data.Dataset):
                     transforms.ToTensor(),
                     transforms.Normalize(self.mean_pix, self.std_pix)
                 ])
-                
+
             self.transform = ContrastiveTransformations(self.transform, n_views=2)
 
             self.data = datasets.__dict__[self.dataset_name.upper()](
                 root=dataset_root, train=self.split == 'train',
                 download=True, transform=self.transform)
-        
-        elif self.dataset_name == 'mnist':
-            self.mean_pix = (0.5,)
-            self.std_pix = (0.5,)
-            
+
+        elif self.dataset_name == 'mnist' or self.dataset_name == 'svhn':
+            self.mean_pix = (0.5,)  # Grayscale mean
+            self.std_pix = (0.5,)   # Grayscale std
+
             if split != 'test':
                 self.transform = transforms.Compose([
-                    transforms.Resize(32),  # Resize to 32x32
+                    transforms.Resize(32),  # Resize to 32x32 for compatibility with CIFAR
                     transforms.RandomResizedCrop(32, scale=(0.8, 1.0)),
                     transforms.RandomApply([
                         transforms.ColorJitter(brightness=0.5, contrast=0.5)
@@ -75,27 +67,31 @@ class SimCLRDataset(data.Dataset):
                     transforms.RandomRotation(10),
                     transforms.GaussianBlur(kernel_size=9),
                     transforms.ToTensor(),
-                    transforms.Normalize(self.mean_pix, self.std_pix)
+                    transforms.Normalize(self.mean_pix, self.std_pix)  # Apply grayscale normalization
                 ])
             else:
                 self.transform = transforms.Compose([
-                    transforms.Resize(32),  # Resize to 32x32
+                    transforms.Resize(32),  # Resize to 32x32 for compatibility with CIFAR
                     transforms.ToTensor(),
-                    transforms.Normalize(self.mean_pix, self.std_pix)
+                    transforms.Normalize(self.mean_pix, self.std_pix)  # Apply grayscale normalization
                 ])
-                
+
             self.transform = ContrastiveTransformations(self.transform, n_views=2)
 
-            self.data = datasets.MNIST(
-                root=dataset_root, train=self.split == 'train',
-                download=True, transform=self.transform)
-        
+            if self.dataset_name == 'mnist':
+                self.data = datasets.MNIST(
+                    root=dataset_root, train=self.split == 'train',
+                    download=True, transform=self.transform)
+            elif self.dataset_name == 'svhn':
+                self.data = datasets.SVHN(
+                    root=dataset_root, split=self.split,
+                    download=True, transform=self.transform)
         else:
             raise ValueError(f'Not recognized dataset {dataset_name}')
-    
+
     def __getitem__(self, index):
         imgs, label = self.data[index]  # n views of images and label
         return imgs, int(label)
-    
+
     def __len__(self):
         return len(self.data)
